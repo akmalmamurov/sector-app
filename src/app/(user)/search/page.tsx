1d8138ced58959b1050764c9818a46adc9c05fbe
akoobeeEy <akmallmamuroff12@gmail.com>
2025-01-27 12:36:49 +0500

    search suspense

--- src/app/(user)/search/page.tsx
+++ src/app/(user)/search/page.tsx
@@ -1,17 +1,27 @@
 "use client";
+
 import { useSearchParams } from "next/navigation";
+import React, { Suspense } from "react";
+
 const SearchPage = () => {
   const searchParams = useSearchParams();
-
   const search = searchParams.get("query");
 
   return (
     <div>
       <h1 className="text-2xl font-bold text-center text-blue-600 uppercase">
-        {search}
+        {search || "No query provided"}
       </h1>
     </div>
   );
 };
 
-export default SearchPage;
+const SearchPageWrapper = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchPage />
+    </Suspense>
+  );
+};
+
+export default SearchPageWrapper; 
