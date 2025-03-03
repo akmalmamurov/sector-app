b8674d7e243da628f3322cc31c36bc34b6439f26
akoobeeEy <akmallmamuroff12@gmail.com>
2025-03-03 20:14:12 +0500

    re pushed

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { getCatalog } from "@/api/catalog";
 import { CatalogChevronIcon } from "@/assets/icons";
 import { HomeCrumb } from "@/components/bread-crumb";
@@ -6,7 +8,6 @@ import { InfoHeader } from "@/components/div";
 import { Section } from "@/components/section";
 import { InfoTitle } from "@/components/title";
 import { CatalogData } from "@/types";
-import Link from "next/link";
 
 const CatalogPage = async () => {
   const catalogData = await getCatalog();
@@ -19,27 +20,27 @@ const CatalogPage = async () => {
           <InfoTitle>Каталог товаров</InfoTitle>
         </InfoHeader>
         <div className="p-6 grid grid-cols-2">
-          {catalogData.map((item: CatalogData) => (
-            <div key={item.id} className="mb-[50px]">
+          {catalogData?.map((item: CatalogData) => (
+            <div key={item?.id} className="mb-[50px]">
               <div className="mb-7">
                 <Link
-                  href={`/catalog/${item.slug}`}
+                  href={`/catalog/${item?.slug}`}
                   className="font-normal text-[21px] text-textColor hover:text-celBlue duration-150 ease-in-out"
                 >
-                  {item.title}
+                  {item?.title}
                 </Link>
               </div>
               <div className="flex flex-col gap-2">
                 {item.subcatalogs?.map((subcatalog) => (
                   <Link
-                    key={subcatalog.id}
-                    href={`/catalog/${subcatalog.slug}`}
+                    key={subcatalog?.id}
+                    href={`/catalog/${subcatalog?.slug}`}
                     className="flex items-center text-xs text-textColor hover:text-celBlue duration-150 ease-in-out"
                   >
                     <span className="mr-[4.68px]">
                       <CatalogChevronIcon />
                     </span>
-                    {subcatalog.title}
+                    {subcatalog?.title}
                   </Link>
                 ))}
               </div>
