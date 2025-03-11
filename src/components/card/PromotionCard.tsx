785f8385adf2a71222dd2499a67f4e1eb0301db2
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2025-03-11 15:11:06 +0500

    Fixed promotion cart

--- src/components/card/PromotionCard.tsx
+++ src/components/card/PromotionCard.tsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { PromotionData } from "@/types";
+import { formatDate } from "@/utils/format-date";
 
 export const PromotionCard = ({ promotion }: { promotion: PromotionData }) => {
   return (
     <Link
       href={`/action/${promotion.id}`}
-      className="p-2 rounded-[10px] group shadow-promotionShadow"
+      className="p-2 rounded-[10px] shadow-promotionShadow"
     >
       <div className="overflow-hidden">
         <Image
@@ -17,14 +18,17 @@ export const PromotionCard = ({ promotion }: { promotion: PromotionData }) => {
           width={230}
           height={148}
           loading="lazy"
-          className="object-cover border w-full h-[148px]"
+          className="object-cover w-full h-[148px]"
         />
       </div>
 
-      <div className="flex justify-between gap-2 mt-2 h-[72px] overflow-hidden">
-        <h3 className="font-medium text-sm text-titleColor text-left line-clamp-3 h-fit">
+      <div className="flex flex-col justify-between gap-2 mt-2 overflow-hidden">
+        <h3 className="font-medium text-sm text-textColor text-left line-clamp-3 h-fit">
           {promotion.title}
         </h3>
+        <p className="text-[#FF3333] text-xs font-normal mb-1">
+          Действует до <span>{formatDate(promotion.expireDate)}</span>
+        </p>
       </div>
     </Link>
   );
