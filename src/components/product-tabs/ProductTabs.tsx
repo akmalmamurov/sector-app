c87091dab9d6d9ea3b960583727a66ca792d67eb
Temur_ <145855199+Temur00@users.noreply.github.com>
2025-03-13 20:17:39 +0500

    'Home page responsive completed'

--- src/components/product-tabs/ProductTabs.tsx
+++ src/components/product-tabs/ProductTabs.tsx
@@ -45,12 +45,15 @@ const ProductTabs = () => {
       onValueChange={(value) => setActiveTab(value)}
       className="bg-white shadow-productListShadow"
     >
-      <TabsList className="flex gap-4 border-b h-[54px] justify-between bg-white p-0">
+      <TabsList
+        style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+        className="flex gap-4 border-b h-[54px] justify-between bg-white p-0 overflow-x-auto whitespace-nowrap "
+      >
         {tabs.map((tab) => (
           <TabsTrigger
             key={tab.key}
             value={tab.key}
-            className="relative font-medium text-base data-[state=active]:bg-white w-[208px] text-gray-600 rounded-none data-[state=active]:text-cerulean data-[state=active]:shadow-none transition-all before:absolute before:-bottom-[15px] before:left-0 before:w-full before:h-[5px] before:bg-gradient-to-r before:from-blue-400 before:to-cerulean before:opacity-0 data-[state=active]:before:opacity-100"
+            className="relative font-medium text-base data-[state=active]:bg-white w-[208px] text-gray-600 rounded-none data-[state=active]:text-cerulean data-[state=active]:shadow-none transition-all before:absolute before:-bottom-[15px] before:left-0 before:w-full before:h-[5px] before:bg-gradient-to-r before:from-blue-400 before:to-cerulean before:opacity-0 data-[state=active]:before:opacity-100 mb-[9px]"
           >
             {tab.label}
           </TabsTrigger>
@@ -60,7 +63,7 @@ const ProductTabs = () => {
       <div className="py-[32px]">
         <TabsContent
           value={activeTab}
-          className={`grid  px-5 mt-0 ${activeTab === "promotion" ? "grid-cols-2 lgl:grid-cols-3 gap-7" : "grid-cols-3 lgl:grid-cols-4 gap-4"}`}
+          className={`grid  px-5 mt-0 ${activeTab === "promotion" ? "grid-cols-2 lg:grid-cols-3 gap-7" : "grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"}`}
         >
           {isLoading ? (
             <div>Loading...</div>
