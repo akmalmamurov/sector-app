92e561a2ff45a43c49ec12507e9bd6a2838c2469
akoobeeEy <akmallmamuroff12@gmail.com>
2025-04-15 15:14:04 +0500

    agent-search

--- src/app/profile/contractors/page.tsx
+++ src/app/profile/contractors/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { getAgent } from "@/api";
+import { SearchIcon } from "@/assets/icons";
 import { ContrAgent } from "@/components/contr-agent";
 import { ContrAgentModal } from "@/components/modal";
 import { useQuery } from "@tanstack/react-query";
@@ -9,9 +10,11 @@ import { useState, useEffect } from "react";
 const ContractorsPage = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = () => setIsOpen((prev) => !prev);
+  const [search, setSearch] = useState("");
   const { data: agentsData = [] } = useQuery({
-    queryKey: ["contragents"],
-    queryFn: getAgent,
+    queryKey: ["contragents", search],
+    queryFn: ()=>
+      getAgent(search),
   });
 
   useEffect(() => {
@@ -39,7 +42,20 @@ const ContractorsPage = () => {
 
   return (
     <div>
-      <div className="grid grid-cols-3 border-b border-superSilver pb-8 gap-[22px] w-full ">
+      <div className="relative mb-[33px]">
+        <input
+          type="text"
+          placeholder="Поиск контрагента"
+          className=" w-full h-[41px] px-3 border border-superSilver hover:border-cerulean/70 focus:outline-none
+           focus:ring-1 focus:ring-cerulean focus:border-transparent transition"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <span className="absolute right-3 top-1/2 transform -translate-y-1/2">
+          <SearchIcon/>
+        </span>
+      </div>
+      <div className="grid grid-cols-4 border-b border-superSilver pb-8 gap-[22px] w-full ">
         <div className="flex flex-col">
           <button
             type="button"
@@ -51,7 +67,7 @@ const ContractorsPage = () => {
           </button>
         </div>
         {/* get contragents */}
-        <ContrAgent contrAgents={agentsData?.kontragents || []}  />
+        <ContrAgent contrAgents={agentsData?.kontragents || []} />
       </div>
       <ContrAgentModal isOpen={isOpen} toggleOpen={toggleOpen} />
     </div>
--- src/components/contr-agent/ContrAgent.tsx
+++ src/components/contr-agent/ContrAgent.tsx
@@ -16,18 +16,20 @@ import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import request from "@/services";
 import { DELETE_AGENT } from "@/constants";
 import { AgentAdressModal } from "../modal";
+import { cn } from "@/lib/utils";
 
 export const ContrAgent = ({
   contrAgents,
   setValue,
+  className,
 }: {
   contrAgents: ContrAgentData[];
   setValue?: UseFormSetValue<OrderRequest>;
+  className?: string;
 }) => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = () => setIsOpen(!isOpen);
   const queryClient = useQueryClient();
-console.log(contrAgents);
 
   useEffect(() => {
     const fav = contrAgents.find((c) => c.isFavorite);
@@ -61,78 +63,81 @@ console.log(contrAgents);
 
   return (
     <>
-      {contrAgents?.map((item) => (
-        <div
-          key={item.id}
-          className={`border ${
-            item.isFavorite ? "border-cerulean" : "border-superSilver"
-          } relative pt-12 px-4 overflow-hidden bg-custom min-h-[229px]`}
-        >
-          <div className="mb-[15px]">
-            <h5 className="text-textColor text-sm mb-2">{item.name}</h5>
-            <p className="text-weekColor text-xs">ИНН {item.inn}</p>
-          </div>
-
-          <div className="gap-2 flex mb-2">
-            <span className="border-b border-dashed pb-[2px] text-xs text-cerulean border-cerulean">
-              Розничная цена
-            </span>
-            <span
-              onClick={toggleOpen}
-              className="border-b border-dashed pb-[2px] text-xs text-cerulean border-cerulean cursor-pointer"
-            >
-              Адреса отгрузки: 0
-            </span>
-          </div>
-
-          <button
-            type="button"
-            onClick={() => handleUpdate(item.id)}
-            disabled={item.isFavorite}
-            className="mt-[31px] flex gap-3 items-center px-[23px] text-weekColor h-[42px] border disabled:bg-cerulean disabled:text-white disabled:border-none"
+      {contrAgents
+        ?.slice()
+        .sort((a, b) => (b.isFavorite ? 1 : 0)- (a.isFavorite ? 1 : 0))
+        .map((item) => (
+          <div
+            key={item.id}
+            className={cn(`border ${
+              item.isFavorite ? "border-cerulean" : "border-superSilver"
+            } relative pt-12 px-4 overflow-hidden bg-custom min-h-[229px]`,className)}
           >
-            <CheckIcon />
-            {item.isFavorite ? "Выбрано" : "Выбрать"}
-          </button>
-          <Popover>
-            <PopoverTrigger asChild>
-              <button
-                type="button"
-                className="absolute top-4 right-2 cursor-pointer w-fit"
-              >
-                <EditIcon className="w-6 h-6 text-darkSoul hover:text-textColor" />
-              </button>
-            </PopoverTrigger>
-            <PopoverContent className="w-[200px] p-0 rounded-none flex flex-col right-5 absolute">
+            <div className="mb-[15px]">
+              <h5 className="text-textColor text-sm mb-2">{item.name}</h5>
+              <p className="text-weekColor text-xs">ИНН {item.inn}</p>
+            </div>
+
+            <div className="gap-2 flex mb-2">
+              <span className="border-b border-dashed pb-[2px] text-xs text-cerulean border-cerulean">
+                Розничная цена
+              </span>
               <span
                 onClick={toggleOpen}
-                className="cursor-pointer py-[6px] px-4 bg-white text-xs text-textColor hover:bg-superSilver hoverEffect flex items-center gap-2"
+                className="border-b border-dashed pb-[2px] text-xs text-cerulean border-cerulean cursor-pointer"
               >
-                <span>
-                  <ShippingIcon />
-                </span>
                 Адреса отгрузки: 0
               </span>
+            </div>
 
-              <span
-                onClick={() => handleDelete(item.id)}
-                className="cursor-pointer py-[6px] px-4 bg-white text-xs text-textColor hover:bg-superSilver hoverEffect flex items-center gap-2"
-              >
-                <span className="w-4 h-4">
-                  <DeleteSmIcon className="w-4 h-4" />
+            <button
+              type="button"
+              onClick={() => handleUpdate(item.id)}
+              disabled={item.isFavorite}
+              className="mt-[31px] flex gap-3 items-center px-[23px] text-weekColor h-[42px] border disabled:bg-cerulean disabled:text-white disabled:border-none"
+            >
+              <CheckIcon />
+              {item.isFavorite ? "Выбрано" : "Выбрать"}
+            </button>
+            <Popover>
+              <PopoverTrigger asChild>
+                <button
+                  type="button"
+                  className="absolute top-4 right-2 cursor-pointer w-fit"
+                >
+                  <EditIcon className="w-6 h-6 text-darkSoul hover:text-textColor" />
+                </button>
+              </PopoverTrigger>
+              <PopoverContent className="w-[200px] p-0 rounded-none flex flex-col right-5 absolute">
+                <span
+                  onClick={toggleOpen}
+                  className="cursor-pointer py-[6px] px-4 bg-white text-xs text-textColor hover:bg-superSilver hoverEffect flex items-center gap-2"
+                >
+                  <span>
+                    <ShippingIcon />
+                  </span>
+                  Адреса отгрузки: 0
                 </span>
-                Удалить
-              </span>
-            </PopoverContent>
-          </Popover>
-          <AgentAdressModal
-            isOpen={isOpen}
-            toggleOpen={toggleOpen}
-            name={item?.name}
-            contrAgentId={item?.id}
-          />
-        </div>
-      ))}
+
+                <span
+                  onClick={() => handleDelete(item.id)}
+                  className="cursor-pointer py-[6px] px-4 bg-white text-xs text-textColor hover:bg-superSilver hoverEffect flex items-center gap-2"
+                >
+                  <span className="w-4 h-4">
+                    <DeleteSmIcon className="w-4 h-4" />
+                  </span>
+                  Удалить
+                </span>
+              </PopoverContent>
+            </Popover>
+            <AgentAdressModal
+              isOpen={isOpen}
+              toggleOpen={toggleOpen}
+              name={item?.name}
+              contrAgentId={item?.id}
+            />
+          </div>
+        ))}
     </>
   );
 };
