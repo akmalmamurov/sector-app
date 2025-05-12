c7253ed4b1dbaf4d051fa17904d785af85e1e90b
akoobeeEy <akmallmamuroff12@gmail.com>
2025-05-12 10:03:09 +0500

    phone modal

--- src/components/modal/PhoneModal.tsx
+++ src/components/modal/PhoneModal.tsx
@@ -1,4 +1,4 @@
-import { X } from "lucide-react";
+import { Phone, X } from "lucide-react";
 import {
   Dialog,
   DialogContent,
@@ -7,11 +7,18 @@ import {
   DialogTitle,
 } from "../ui/dialog";
 import { Separator } from "../ui/separator";
+import { useState } from "react";
+import { PhoneInput } from "../phone-input";
 interface Props {
   isOpen: boolean;
   toggleOpen: () => void;
 }
 export const PhoneModal = ({ isOpen, toggleOpen }: Props) => {
+  const [phone, setPhone] = useState<string>("+998 __ ___ ____");
+  const [error, setError] = useState(false);
+  const [isPhoneComplete, setIsPhoneComplete] = useState(false);
+  console.log(phone.length);
+
   return (
     <Dialog open={isOpen} onOpenChange={toggleOpen}>
       <DialogContent className="max-w-[600px] sm:rounded-none p-0 border-none">
@@ -28,7 +35,35 @@ export const PhoneModal = ({ isOpen, toggleOpen }: Props) => {
         <DialogDescription className="hidden">asd</DialogDescription>
         <Separator className=" h-[0.5px]" />
         <div className="px-[23px] pt-1 pb-[23px]">
-                asdasdasdsa
+          <div className="relative">
+            <PhoneInput
+              id="phoneNumber"
+              value={phone}
+              onChange={setPhone}
+              onComplete={() => setIsPhoneComplete(true)}
+              onIncomplete={() => setIsPhoneComplete(false)}
+              className={`border ${
+                error
+                  ? "border-dangerColor hover:border-dangerColor/50"
+                  : "border-superSilver hover:border-cerulean/50"
+              } py-2 pl-[15px] pr-[47px] w-full focus:outline-none focus:border-cerulean`}
+            />
+            <span className="absolute right-0 top-1/2 transform -translate-y-1/2 pr-3">
+              <Phone
+                className={
+                  isPhoneComplete ? "text-cerulean" : "text-superSilver"
+                }
+              />
+            </span>
+          </div>
+          <div className="mt-10 flex justify-end">
+            <button
+              disabled={!isPhoneComplete}
+              className="py-2 px-[23px] disabled:bg-superSilver bg-cerulean text-white disabled:text-darkSoul font-semibold"
+            >
+              Сохранить
+            </button>
+          </div>
         </div>
       </DialogContent>
     </Dialog>
--- src/components/phone-input/PhoneInput.tsx
+++ src/components/phone-input/PhoneInput.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import React from "react";
+import React, { useState } from "react";
 import { IMaskInput } from "react-imask";
 
 interface PhoneInputProps
@@ -10,28 +8,55 @@ interface PhoneInputProps
   > {
   value: string;
   onChange: (val: string) => void;
+  onComplete?: () => void;
+  onIncomplete?: () => void;
   id?: string;
   className?: string;
   defaultValue?: string | number;
 }
 
 export const PhoneInput = React.forwardRef<HTMLInputElement, PhoneInputProps>(
-  ({ value, onChange, id, className, defaultValue, ...rest }, ref) => (
-    <IMaskInput
-      mask={"+998 00 000 0000"}
-      definitions={{ "0": /\d/ }}
-      unmask={false}
-      placeholderChar="_"
-      lazy={false}
-      defaultValue={defaultValue}
-      value={value}
-      onAccept={(val: string) => onChange(val)}
-      inputRef={ref}
-      id={id}
-      className={className}
-      {...rest}
-    />
-  )
+  (
+    {
+      value,
+      onChange,
+      onComplete,
+      onIncomplete,
+      id,
+      className,
+      defaultValue,
+      ...rest
+    },
+    ref
+  ) => {
+    const [_isComplete, setIsComplete] = useState(false);
+
+    return (
+      <IMaskInput
+        mask={"+998 00 000 0000"}
+        definitions={{ "0": /\d/ }}
+        unmask={false}
+        placeholderChar="_"
+        lazy={false}
+        defaultValue={defaultValue}
+        value={value}
+        onAccept={(val: string, mask) => {
+          onChange(val);
+          if (mask.masked.isComplete) {
+            setIsComplete(true);
+            onComplete?.();
+          } else {
+            setIsComplete(false);
+            onIncomplete?.();
+          }
+        }}
+        inputRef={ref}
+        id={id}
+        className={className}
+        {...rest}
+      />
+    );
+  }
 );
 
 PhoneInput.displayName = "PhoneInput";
